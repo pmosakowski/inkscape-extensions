63d63ed56b790166b4250c3f82e82cbcae02be07
pmosakowski <pmosakowski@users.noreply.github.com>
2015-09-14 14:02:19 +0100

    csv_to_vinyl: now can render names from a list

--- csv_to_vinyl.py
+++ csv_to_vinyl.py
@@ -16,6 +16,7 @@ class CsvToVinyl(InkscapeEffect):
         self.OptionParser.add_option('-f', '--csv_file', action = 'store',
             type=str, dest = 'csv_file', default = '/home',
             help = 'CSV file to read names from')
+        self.names = names
         self.filename = sys.argv[-1]
 
     def effect(self):
@@ -23,21 +24,16 @@ class CsvToVinyl(InkscapeEffect):
         self.csv_file = self.options.csv_file
 
         if len(self.selected) == 1:
+            # extract style from selection
             self.text_style = self.selected.popitem()[1].attrib["style"]
             assert self.text_style != None
-            self.add_name('Pawel Mosakowski')
+            for name in self.names:
+                self.add_name(name)
 
     def add_name(self, name):
-        #parent = self.current_layer
-        parent = self.root
-
-        style = {
-            'text-align' : 'center', 
-            'text-anchor' : 'middle',
-        }
+        parent = self.current_layer
 
         text_attribs = {
-            #'style' : simplestyle.formatStyle(style),
             'style' : self.text_style,
             'x' : str(0),
             'y' : str(0),
@@ -50,13 +46,13 @@ class CsvToVinyl(InkscapeEffect):
             addNS('role','sodipodi') : 'line',
         }
 
-        # turn whitespace into linebreak
+        # make all-caps and turn whitespace into linebreak
+        name = name.upper()
         firstname = name.split()[0]
         lastname = name.split()[1]
         
         # add nodes to doucument tree
         text = etree.SubElement(parent, addNS('text','svg'), attrib=text_attribs)
-        #text = etree.SubElement(parent, addNS('text','svg'), attrib=text_attribs)
         first_line = etree.SubElement(text, addNS('tspan','svg'), line_attribs)
         first_line.text = firstname
         second_line = etree.SubElement(text, addNS('tspan','svg'), line_attribs)
