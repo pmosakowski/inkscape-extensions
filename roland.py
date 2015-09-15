44cace241f22e27a4f1c685dba4592fe9f5d97ee
pmosakowski <pmosakowski@users.noreply.github.com>
2015-09-15 11:41:26 +0100

    roland: draw bounding boxes around selected nodes

--- roland.py
+++ roland.py
@@ -17,14 +17,21 @@ class Roland(InkscapeEffect):
     def effect(self):
         if len(self.selected) > 0:
             svg = self.document.getroot()
-            self.calculate_bboxes(self.selected)
-            self.draw_rect(0, 0, 100, 100)
-    
+            bboxes = self.calculate_bboxes(self.selected)
+            for bbox in bboxes:
+                self.draw_bbox(bbox)
+
     @staticmethod
     def calculate_bboxes(nodes):
         bboxes = [computeBBox([node]) for id, node in nodes.items()]
         return bboxes
     
+    def draw_bbox(self, bbox):
+        (x1, x2, y1, y2) = bbox
+        width = x2 - x1
+        height = y2 - y1
+        self.draw_rect(x1, y1, width, height)
+
     #SVG element generation routine
     def draw_rect(self, x, y, width, height):
         layer = self.current_layer
